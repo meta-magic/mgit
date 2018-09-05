9d2140039189ddc9fca229029a82b3053b1a52e3
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-09-05 12:51:07 +0530

    Mgit helper updated

--- mGit/src/main/java/com/desire3d/mgit/helper/MGitHelper.java
+++ mGit/src/main/java/com/desire3d/mgit/helper/MGitHelper.java
@@ -34,19 +34,23 @@ import com.desire3d.mgit.exceptions.PullFailureException;
  *
  */
 public abstract class MGitHelper {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(MGitHelper.class);
-	
-	private static final String GIT_DEFAULT_DIRECTORY = ".git"; 
-	
+
+	private static final String GIT_DEFAULT_DIRECTORY = ".git";
+
 	/**
-	 * method to clone repository with authentication with {@link UsernamePasswordCredentialsProvider}
+	 * method to clone repository with authentication with
+	 * {@link UsernamePasswordCredentialsProvider}
 	 * 
-	 * @param remoteURL remote url of repository eg. https://github.com/meta-magic/mgittestrepo.git
-	 * @param localDir directory in which git repository is configured  
+	 * @param remoteURL
+	 *            remote url of repository eg.
+	 *            https://github.com/meta-magic/mgittestrepo.git
+	 * @param localDir
+	 *            directory in which git repository is configured
 	 * @return {@link Git}
-	 * @throws CloneFailureException 
-	 * */
+	 * @throws CloneFailureException
+	 */
 	public static Git cloneRepository(final String remoteURL, final File localDir) throws CloneFailureException {
 		CloneRepository repo = new CloneRepository();
 		Git clonedGit;
@@ -54,78 +58,140 @@ public abstract class MGitHelper {
 			clonedGit = repo.cloneRepository(remoteURL, localDir);
 		} catch (GitAPIException e) {
 			e.printStackTrace();
-			throw new CloneFailureException("Git remote repository cloning failed due to cause - "+ e.getMessage(), e);
+			throw new CloneFailureException("Git remote repository cloning failed due to cause - " + e.getMessage(), e);
 		}
 		return clonedGit;
 	}
-	
+
 	/**
-	 * method to clone repository with authentication with {@link UsernamePasswordCredentialsProvider}
+	 * method to clone repository with authentication with
+	 * {@link UsernamePasswordCredentialsProvider}
 	 * 
-	 * @param remoteURL remote url of repository eg. https://github.com/meta-magic/mgittestrepo.git
-	 * @param localDir directory in which git repository is configured  
+	 * @param remoteURL
+	 *            remote url of repository eg.
+	 *            https://github.com/meta-magic/mgittestrepo.git
+	 * @param localDir
+	 *            directory in which git repository is configured
 	 * @return {@link Git}
-	 * @throws CloneFailureException 
-	 * */
-	public static Git cloneRepository(final String remoteURL, final String username, final String password, final File localDir) throws CloneFailureException {
+	 * @throws CloneFailureException
+	 */
+	public static Git cloneRepository(final String remoteURL, final String username, final String password,
+			final File localDir) throws CloneFailureException {
 		CloneRepository repo = new CloneRepository();
 		Git clonedGit;
 		try {
 			clonedGit = repo.cloneRepository(remoteURL, username, password, localDir);
 		} catch (GitAPIException e) {
 			e.printStackTrace();
-			throw new CloneFailureException("Git remote repository cloning failed due to cause - "+ e.getMessage(), e);
+			throw new CloneFailureException("Git remote repository cloning failed due to cause - " + e.getMessage(), e);
 		}
 		return clonedGit;
 	}
-	
-	/** 
-	 * method to read and open existing local repository and return {@link Repository} object
+
+	/**
+	 * method to read and open existing local repository and return
+	 * {@link Repository} object
 	 * 
-	 * @param localDir directory in which git repository is configured
-	 * @throws LocalRepositoryException 
-	 * */
+	 * @param localDir
+	 *            directory in which git repository is configured
+	 * @throws LocalRepositoryException
+	 */
 	public static Repository openRepository(final String localDir) throws LocalRepositoryException {
 		FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
 		Repository repository;
 		try {
-			repository = repositoryBuilder.setGitDir(new File(appendGitDefaultDir(localDir)))
-										  .readEnvironment() 
-										  .findGitDir()
-										  .setMustExist(true)
-										  .build();
+			repository = repositoryBuilder.setGitDir(new File(appendGitDefaultDir(localDir))).readEnvironment()
+					.findGitDir().setMustExist(true).build();
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new LocalRepositoryException("Git local repository open failed due to cause - " + e.getMessage(), e);
 		}
 		return repository;
 	}
-	
+
+	public static void delete(File file) throws IOException {
+		if (file.isDirectory()) {
+			// directory is empty, then delete it
+			if (file.list().length == 0) {
+				file.delete();
+			} else {
+				// list all the directory contents
+				String files[] = file.list();
+				for (String temp : files) {
+					// construct the file structure
+					File fileDelete = new File(file, temp);
+
+					// recursive delete
+					delete(fileDelete);
+				}
+
+				// check the directory again, if empty then delete it
+				if (file.list().length == 0) {
+					file.delete();
+				}
+			}
+
+		} else {
+			// if file, then delete it
+			file.delete();
+		}
+	}
+
 	/**
-	 * Initial commit to the repository includes cloning remote repository and first commit of project
+	 * Initial commit to the repository includes cloning remote repository and first
+	 * commit of project
 	 * 
-	 * @param remoteURL of github repository
-	 * @param username of github user
-	 * @param password of github user account 
-	 * @param commitMessage 
-	 * @param localDir directory in which git repository is configured 
-	 * @throws LocalRepositoryException, BaseMGitAPIException 
+	 * @param remoteURL
+	 *            of github repository
+	 * @param username
+	 *            of github user
+	 * @param password
+	 *            of github user account
+	 * @param commitMessage
+	 * @param localDir
+	 *            directory in which git repository is configured
+	 * @throws LocalRepositoryException,
+	 *             BaseMGitAPIException
 	 * 
-	 * */
-	public static void initialCommit(final String remoteURL, final String username, final String password, final String commitMessage, final String localDir) throws LocalRepositoryException, BaseMGitAPIException  {
+	 */
+	public static void initialCommit(final String remoteURL, final String username, final String password,
+			final String commitMessage, final String localDir) throws LocalRepositoryException, BaseMGitAPIException {
 		File tempFolder = null;
 		try {
 			tempFolder = Files.createTempDirectory("TempCloningDir").toFile();
 			cloneRepository(remoteURL, username, password, tempFolder);
+			// File file = new File((localDir) + "/.git");
+			if (localDir != null) {
 
-			Files.move(
-					   Paths.get(appendGitDefaultDir(tempFolder.getAbsolutePath())), 
-					   Paths.get(appendGitDefaultDir(localDir)), 
-					   StandardCopyOption.REPLACE_EXISTING
-			);
+				File directory = new File((localDir) + "/.git");
+
+				// make sure directory exists
+				if (directory.exists()) {
+
+					try {
+
+						delete(directory);
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				Files.move(Paths.get(appendGitDefaultDir(tempFolder.getAbsolutePath())),
+						Paths.get(appendGitDefaultDir(localDir)), StandardCopyOption.REPLACE_EXISTING);
+
+			} else {
+				Files.move(Paths.get(appendGitDefaultDir(tempFolder.getAbsolutePath())),
+						Paths.get(appendGitDefaultDir(localDir)), StandardCopyOption.REPLACE_EXISTING);
+			}
+			// Files.move(Paths.get(appendGitDefaultDir(tempFolder.getAbsolutePath())),
+			// Paths.get(appendGitDefaultDir(localDir)),
+			// StandardCopyOption.REPLACE_EXISTING);
 			LOGGER.info("Cloned repository configured to project at " + localDir);
 			commitAllAndPush(username, password, commitMessage, localDir);
-		} catch (Throwable e) {
+		} catch (
+
+		Throwable e) {
 			e.printStackTrace();
 			if (e instanceof BaseMGitAPIException) {
 				throw (BaseMGitAPIException) e;
@@ -133,23 +199,27 @@ public abstract class MGitHelper {
 			throw new LocalRepositoryException(e.getMessage(), e);
 		}
 	}
-	
+
 	/**
-	 * Call to index all changes, commit locally and push to remote repository 
+	 * Call to index all changes, commit locally and push to remote repository
 	 * 
-	 * @param username of github user
-	 * @param password of github user account 
-	 * @param commitMessage 
-	 * @param localDir directory in which git repository is configured
-	 * @throws BaseMGitAPIException 
-	 * */
-	public static void commitAllAndPush(final String username, final String password, final String commitMessage, final String localDir) throws CommitFailureException, BaseMGitAPIException {
+	 * @param username
+	 *            of github user
+	 * @param password
+	 *            of github user account
+	 * @param commitMessage
+	 * @param localDir
+	 *            directory in which git repository is configured
+	 * @throws BaseMGitAPIException
+	 */
+	public static void commitAllAndPush(final String username, final String password, final String commitMessage,
+			final String localDir) throws CommitFailureException, BaseMGitAPIException {
 		try {
 			IndexFiles indexFiles = new IndexFiles();
 			Git git = indexFiles.addAllFilesToIndex(localDir);
 			new RepositoryStatus().getFilesAddedToIndex(git);
 			Commit commit = new Commit();
-			Iterable<PushResult> pushresult = commit.commitAllAndPush(git, commitMessage, username, password);	
+			Iterable<PushResult> pushresult = commit.commitAllAndPush(git, commitMessage, username, password);
 			for (final PushResult result : pushresult) {
 				for (final RemoteRefUpdate upd : result.getRemoteUpdates()) {
 					LOGGER.info(upd.getStatus());
@@ -163,18 +233,23 @@ public abstract class MGitHelper {
 			throw new CommitFailureException("Commit all & Push failed due to cause - " + e.getMessage(), e);
 		}
 	}
-	
+
 	/**
-	 * Call to index selected files, commit locally and push to remote repository 
+	 * Call to index selected files, commit locally and push to remote repository
 	 * 
-	 * @param username of github user
-	 * @param password of github user account 
-	 * @param commitMessage 
-	 * @param localDir directory in which git repository is configured 
-	 * @param filePaths selected files relative path
-	 * @throws CommitFailureException 
-	 * */
-	public static void commitSelectedAndPush(final String username, final String password, final String commitMessage, final String localDir, final Set<String> filePaths) throws CommitFailureException, BaseMGitAPIException {
+	 * @param username
+	 *            of github user
+	 * @param password
+	 *            of github user account
+	 * @param commitMessage
+	 * @param localDir
+	 *            directory in which git repository is configured
+	 * @param filePaths
+	 *            selected files relative path
+	 * @throws CommitFailureException
+	 */
+	public static void commitSelectedAndPush(final String username, final String password, final String commitMessage,
+			final String localDir, final Set<String> filePaths) throws CommitFailureException, BaseMGitAPIException {
 		try {
 			IndexFiles indexFiles = new IndexFiles();
 			Git git = indexFiles.addAllFilesToIndex(localDir, filePaths);
@@ -183,7 +258,7 @@ public abstract class MGitHelper {
 				LOGGER.info(change);
 			}
 			Commit commit = new Commit();
-			Iterable<PushResult> pushresult = commit.commitAllAndPush(git, commitMessage, username, password);	
+			Iterable<PushResult> pushresult = commit.commitAllAndPush(git, commitMessage, username, password);
 			for (final PushResult result : pushresult) {
 				for (final RemoteRefUpdate upd : result.getRemoteUpdates()) {
 					LOGGER.info(upd.getStatus());
@@ -197,17 +272,22 @@ public abstract class MGitHelper {
 			throw new CommitFailureException("Commit all & Push failed due to cause - " + e.getMessage(), e);
 		}
 	}
-	
+
 	/**
-	 * pull changes from remote repository to local repository 
+	 * pull changes from remote repository to local repository
 	 * 
-	 * @param username - committer's github username
-	 * @param password - committer's github password
-	 * @param localDir directory in which git repository is configured
-	 * @throws PullFailureException, BaseMGitAPIException 
+	 * @param username
+	 *            - committer's github username
+	 * @param password
+	 *            - committer's github password
+	 * @param localDir
+	 *            directory in which git repository is configured
+	 * @throws PullFailureException,
+	 *             BaseMGitAPIException
 	 * 
-	 * */
-	public static PullResult pull(final String username, final String password, final String localDir) throws PullFailureException, BaseMGitAPIException {
+	 */
+	public static PullResult pull(final String username, final String password, final String localDir)
+			throws PullFailureException, BaseMGitAPIException {
 		PullRequest pullRequest = new PullRequest();
 		try {
 			return pullRequest.pull(localDir, username, password);
@@ -219,34 +299,38 @@ public abstract class MGitHelper {
 			throw new PullFailureException("PullRequest request failed due to cause - " + e.getMessage(), e);
 		}
 	}
-	
+
 	/**
 	 * Get uncommitted changes
 	 * 
-	 * @param localDir directory in which git repository is configured
+	 * @param localDir
+	 *            directory in which git repository is configured
 	 * @return uncommitted changes
-	 * @throws LocalRepositoryException 
-	 * */
-	public static Map<String, Set<String>> getUncommittedChanges(final String localDir) throws LocalRepositoryException {
+	 * @throws LocalRepositoryException
+	 */
+	public static Map<String, Set<String>> getUncommittedChanges(final String localDir)
+			throws LocalRepositoryException {
 		try {
 			return new RepositoryStatus().getUncommittedChanges(localDir);
 		} catch (Throwable e) {
 			e.printStackTrace();
-			throw new LocalRepositoryException("Local repository tracking uncommitted changes failed due to cause - " + e.getMessage(), e);
+			throw new LocalRepositoryException(
+					"Local repository tracking uncommitted changes failed due to cause - " + e.getMessage(), e);
 		}
 	}
-	
+
 	/**
-	 * Append the default git directory path to local dir 
+	 * Append the default git directory path to local dir
 	 * 
-	 * @param localDir directory in which git repository is configured 
+	 * @param localDir
+	 *            directory in which git repository is configured
 	 * @return appended absolute path of .git directory
-	 * */
+	 */
 	private static String appendGitDefaultDir(final String localDir) {
 		StringBuilder sb = new StringBuilder(localDir);
 		sb.append("/");
 		sb.append(GIT_DEFAULT_DIRECTORY);
 		return sb.toString();
 	}
-	
+
 }
